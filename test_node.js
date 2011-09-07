2aba7a9fa996bd2624c600466e25f034bf692382
unknown <messuti.edd@.(none)>
2011-09-07 13:56:59 -0400

    fiz

--- test_node.js
+++ test_node.js
@@ -1,10 +1,12 @@
-var http = require('http');
-var port = process.env.PORT || 3000;
+var express = require('express');
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
